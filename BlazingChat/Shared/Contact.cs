37775cdcbcc9ce3b5c333ced87856907a2a6c6c1
adrisel <elecadrinf@gmail.com>
2020-07-10 16:14:24 -0400

    the folder structure added after to create a blazor webassembly app based on visual studio template

---
+++ BlazingChat/Shared/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazingChat.Shared
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        public Contact(int id, string name, string lastName) {
+            this.ID = id;
+            this.Name = name;
+            this.LastName = lastName;
+        }
+    }
+}
